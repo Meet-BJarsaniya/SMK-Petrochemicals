5431601f67272daf308de03292e1086cca19a7a3
Meet-BJarsaniya <meet@sanskartechnolab.com>
2024-06-10 14:51:10 +0530

    client scripts, hooks

--- smk_petrochemicals/hooks.py
+++ smk_petrochemicals/hooks.py
@@ -227,3 +227,12 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+fixtures=[
+    {"dt": "Client Script", "filters":[
+        [
+            "module", "in",[
+                "SMK Petrochemicals"
+            ]
+        ]
+    ]},
+]
